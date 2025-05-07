70e95fd902bd4d006d91937873ce47cb4c911eb2
Lucão <lucasyt710@gmail.com>
2025-05-07 17:01:25 -0300

    Implementação do Firebase: configuração do projeto, adição de dependências e inicialização do Firebase no app

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:ponto/view/home.dart';
+import 'package:firebase_core/firebase_core.dart';
 
-void main() {
-  runApp(const MyApp());
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
