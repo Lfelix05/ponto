e296aefdbb44beb58035d4f5741a579f443a6af1
Lucão <lucasyt710@gmail.com>
2025-05-08 15:59:59 -0300

    Revert "Implementação do Firebase: configuração do projeto, adição de dependências e inicialização do Firebase no app"
    This reverts commit 70e95fd902bd4d006d91937873ce47cb4c911eb2.

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -3,7 +3,6 @@ plugins {
     id("kotlin-android")
     // The Flutter Gradle Plugin must be applied after the Android and Kotlin Gradle plugins.
     id("dev.flutter.flutter-gradle-plugin")
-    id("com.google.gms.google-services") version "4.4.2" apply false
 }
 
 android {
@@ -22,7 +21,7 @@ android {
 
     defaultConfig {
         // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
-        applicationId = "com.projeto.ponto"
+        applicationId = "com.example.ponto"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
@@ -38,10 +37,8 @@ android {
             signingConfig = signingConfigs.getByName("debug")
         }
     }
-    
 }
 
 flutter {
     source = "../.."
 }
-apply plugin: 'com.google.gms.google-services'
--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:ponto/view/home.dart';
-import 'package:firebase_core/firebase_core.dart';
 
-void main() async {
-  WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp();
-  runApp(MyApp());
+void main() {
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
